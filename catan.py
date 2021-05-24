0796d158837e153f3020a0644fcebad2e8c8e2d3
meir-poccin <83933438+meir-poccin@users.noreply.github.com>
2021-05-23 21:39:36 -0400

    Update catan.py

--- catan.py
+++ catan.py
@@ -86,7 +86,7 @@ class CatanBoard:
             self.board_resources[zero_tile_nr]
 
         # bank resources  "brick", "ore", "hay", "wood", "sheep"
-        self.bank = cards.Res_cards.cards  #np.array([19, 19, 19, 19])
+        self.bank = cards.Res_cards().cards  #np.array([19, 19, 19, 19])
         # player_points player0, player1, player2, player3
         self.player_points = [0, 0, 0, 0]
         # longest road player_number initialisation with -1
