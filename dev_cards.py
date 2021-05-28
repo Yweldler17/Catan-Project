7dacb3bfc052fafa795172ae6bbbd96a23e39d92
meir-poccin <83933438+meir-poccin@users.noreply.github.com>
2021-05-28 01:21:41 -0400

    Update dev_cards.py

--- dev_cards.py
+++ dev_cards.py
@@ -1,1 +1,25 @@
+DEVELOPMENT_CARD_NAMES = ["knight", "victory point",
+                          "road building", "year of plenty", "monopoly"]
 
+
+class DevCards:
+    def __init__(self):
+        self.cards = {}
+        self.cards[0] = 14
+        self.cards[1] = 5
+        for i in range(2,5):
+            self.cards[i] = 2
+
+    def remove_from_bank(self, card):
+        self.cards[card] -= 1
+
+    def __str__(self):
+        a = 'The bank has:\n'
+        for i,j in self.cards.items():        
+            a += str(j) + ' '
+            a += DEVELOPMENT_CARD_NAMES[i]+ '\n'
+        return a
+
+        
+
+print(DevCards())
